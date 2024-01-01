e20d49726f0506518cf909d3d5647a79594a08c9
Tushar <30565750+tushar5526@users.noreply.github.com>
2024-01-01 14:59:12 +0530

    test

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from utils import comment_on_gh_pr, deploy
 
 def main() -> None:
     event_name = os.environ.get("GITHUB_EVENT_NAME")
+    github_event = os.environ.get("INPUT_GITHUB_EVENT")
+    print(github_event)
     project_url = f"https://github.com/{os.environ.get('INPUT_FORK_REPO_URL') or os.environ.get('GITHUB_REPOSITORY')}.git"
     branch_name = (
         os.environ.get("GITHUB_HEAD_REF")
@@ -21,13 +23,15 @@ def main() -> None:
         sarthi_server_url=sarthi_server_url,
     )
 
-    if event_name != "pull_request":
-        print(service_urls)
-        return
-
     comment = ""
     for urls in service_urls:
         comment += urls + "\n"
+
+    if event_name == "push":
+        print("Deployed Service URL: \n" + comment)
+        return
+
+
     comment_on_gh_pr(f"Deployed service URLs\n" + comment)
 
 
