5a48b383c63be328d7ad5f864803f89dc966415c
Tushar <30565750+tushar5526@users.noreply.github.com>
2024-05-22 16:05:28 +0530

    use gh pat token

--- utils.py
+++ utils.py
@@ -22,6 +22,7 @@ class GitHubHelper:
         else os.environ.get("GITHUB_REF_NAME")
     )
     github_token = os.environ.get("INPUT_REPO_TOKEN")
+    gh_repo_rw_token = os.environ.get("GITHUB_REPO_RW_TOKEN")
     github_api_url = os.environ.get("GITHUB_API_URL")
     github_base_repository = os.environ.get("GITHUB_REPOSITORY")
     pr_number = event_payload.get("number", "-1")
@@ -154,7 +155,7 @@ def handle_pr_events():
         services_url = SarthiHelper.deploy_preview(
             GitHubHelper.get_project_url(),
             GitHubHelper.branch_name,
-            GitHubHelper.github_token
+            GitHubHelper.gh_repo_rw_token
         )
         GitHubHelper.comment_on_gh_pr(
             f"Deployed Services Successfully ✅\n"
